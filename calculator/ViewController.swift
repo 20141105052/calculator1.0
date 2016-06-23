1b82c873d9ca598b8edc7f2e7aff1cd8216072e4
20141105052 <xiaoods3on1@sina.com>
2016-06-23 11:17:33 +0800

    swift－calculator第一次提交
    通过使用swift语句，编辑简易的计算器，使之实现简易的加减乘除等运算，通过强制转换将其转换为double类型，使用switch多分支语句，实现
    计算器的加减乘除，学习了在Xcode编译环境下对计算器的设计

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -19,7 +19,105 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBOutlet weak var x: UITextField!
+    var flag=0
+    var temp=0.0
 
+    @IBAction func one(sender: AnyObject) {
+        x.text=x.text!+"1"
+    }
+    @IBAction func two(sender: AnyObject) {
+        x.text=x.text!+"2"
+    }
+    @IBAction func three(sender: AnyObject) {
+        x.text=x.text!+"3"
+    }
+    @IBAction func four(sender: AnyObject) {
+        x.text=x.text!+"4"
+    }
+    @IBAction func five(sender: AnyObject) {
+        x.text=x.text!+"5"
+    }
+    @IBAction func six(sender: AnyObject) {
+        x.text=x.text!+"6"
+    }
+    @IBAction func seven(sender: AnyObject) {
+        x.text=x.text!+"7"
+    }
+    @IBAction func eight(sender: AnyObject) {
+        x.text=x.text!+"8"
+    }
+    @IBAction func nine(sender: AnyObject) {
+        x.text=x.text!+"9"
+    }
+    @IBAction func zero(sender: AnyObject) {
+        x.text=x.text!+"0"
+    }
+
+    @IBAction func add(sender: AnyObject) {
+        if flag==0{
+            temp=(Double)(x.text!)!
+            x.text=""
+            flag=1
+        }
+        flag=1
+    }
+    @IBAction func jian(sender: AnyObject) {
+        if flag==0{
+            temp=(Double)(x.text!)!
+            x.text=""
+            flag=2
+        }
+        flag=2
+    }
+    @IBAction func mul(sender: AnyObject) {
+        if flag==0{
+            temp=(Double)(x.text!)!
+            x.text=""
+            flag=3
+        }
+        flag=3
+        
+    }
+    @IBAction func chu(sender: AnyObject) {
+        if flag==0{
+            temp=(Double)(x.text!)!
+            x.text=""
+            flag=3
+        }
+        flag=4
+    }
+
+    
+
+    @IBAction func calculator(sender: AnyObject) {
+        switch flag{
+        case 1:
+            temp=temp+(Double)(x.text!)!
+            x.text="\(temp)"
+            break
+        case 2:
+            temp=temp-(Double)(x.text!)!
+            x.text="\(temp)"
+            break
+        case 3:
+            temp=temp*(Double)(x.text!)!
+            x.text="\(temp)"
+            break
+        case 4:
+            temp=temp/(Double)(x.text!)!
+            x.text="\(temp)"
+            break
+
+        default:
+            break
+        }
+    }
+    @IBAction func clear(sender: AnyObject) {
+        temp=0
+        x.text=""
+        flag=0
+    }
 
 }
 
